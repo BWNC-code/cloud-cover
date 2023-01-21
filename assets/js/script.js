21d2c3a316508aaec11e72270e9cb6aa88c83ee7
BWNC-code <bryn.wheeler@gmail.com>
2023-01-21 14:10:51 +0000

    Add weather icon from Open Weather and log all data to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,22 @@ const locationName = document.querySelector('#location-name');
 const forecastImage = document.querySelector('#forecast-image');
 const fourDayForecast = document.querySelector('#day-forecast');
 
+
+
 //Get current weather for a given location
 async function getCurrentWeather(lat, lon) {
     try {
         let currentWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
         let currentWeatherData = await currentWeather.json();
         locationName.textContent = currentWeatherData.name;
+        forecastImage.src = `https://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}.png`;
+        console.log(currentWeatherData);
     }
     catch(error) {
         console.error(error);
     }
 }
+
+//Need function to get lat and lon from form input and run getCurrentWeather
+
+//Function to listen for form submit and run weather update
