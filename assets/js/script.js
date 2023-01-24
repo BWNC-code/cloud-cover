862596138fd6d1aac75180364a353b2474042767
BWNC-code <bryn.wheeler@gmail.com>
2023-01-24 20:40:32 +0000

    Add temperature, weather description and wind speed to current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ const apiKey = 'd5ef31a7c6df783ea28c65895fc0c6d9';
 const locationInput = document.querySelector('#location');
 const locationName = document.querySelector('#location-name');
 const forecastImage = document.querySelector('#forecast-image');
+const temperature = document.querySelector(`#temperature`);
+const weatherDescription = document.querySelector(`#weather-description`);
+const windSpeed = document.querySelector(`#wind-speed`);
 const fourDayForecast = document.querySelector('#day-forecast');
 const errorMessage = document.querySelector("#error-message");
 const geolocateBtn = document.querySelector('#geolocate');
@@ -20,6 +23,9 @@ async function getCurrentWeather(lat, lon) {
         locationName.textContent = currentWeatherData.name;
         forecastImage.src = `https://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}.png`;
         forecastImage.alt = currentWeatherData.weather[0].description;
+        temperature.textContent = `Temperature: ${currentWeatherData.main.temp} °C`;
+        weatherDescription.textContent = `Weather: ${currentWeatherData.weather[0].description}`;
+        windSpeed.textContent = `Wind speed: ${currentWeatherData.wind.speed} m/s`;
         console.log(currentWeatherData);
     } catch (error) {
         console.error(error);
@@ -34,11 +40,13 @@ async function getCurrentWeather(lat, lon) {
 async function getLatLon(location) {
     try {
         let latLon = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`);
+        //Validate response
         if (latLon.status === 404 || latLon.status === 400) {
             throw new Error(`Error code: ${latLon.status}`);
         }
         let latLonData = await latLon.json();
-        if(!latLonData || !latLonData[0].lat || !latLonData[0].lon) {
+        //Validate json
+        if (!latLonData || !latLonData[0].lat || !latLonData[0].lon) {
             throw new Error(`Invalid location: ${location}`);
         }
         return {
@@ -56,12 +64,12 @@ async function getLatLon(location) {
  */
 async function displayWeather(location) {
     try {
-      const coordinates = await getLatLon(location);
-      await getCurrentWeather(coordinates.lat, coordinates.lon);
+        const coordinates = await getLatLon(location);
+        await getCurrentWeather(coordinates.lat, coordinates.lon);
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
-  }
+}
 
 
 //Function to listen for form submit and run weather update
@@ -75,7 +83,6 @@ document.querySelector('form').addEventListener('submit', event => {
 
 geolocateBtn.addEventListener('click', async () => {
     navigator.geolocation.getCurrentPosition(async position => {
-        console.log(position)
         let lat = position.coords.latitude;
         let lon = position.coords.longitude;
         await getCurrentWeather(lat, lon);
